85cf5ebbd4ae8c5930415267d098e809c9a49694
Simon Wicky <simon.wicky@epfl.ch>
2020-05-22 13:48:41 +0200

    change filter function

--- skeleton/pcap.py
+++ skeleton/pcap.py
@@ -1,33 +1,38 @@
 from scapy.all import *
-from pcaper import PcapParser
 import sys
 import os
 import re
 
 
-
-
-def filter(remove):
+def filter2(remove):
     #None to account cell number from 1 to 100
     nb_poi = [None, 11, 14, 13, 5, 11, 6, 10, 12, 8, 5, 12, 9, 12, 9, 14, 11, 15, 10, 10, 7, 6, 10, 9, 10, 15, 13, 8, 8, 12, 8, 11, 8, 7, 9, 7, 15, 11, 12, 11, 3, 15, 7, 8, 14, 12, 10, 11, 6, 7, 8, 14, 9, 9, 5, 9, 11, 13, 10, 7, 12, 12, 11, 8, 15, 10, 11, 8, 6, 13, 12, 9, 6, 14, 13, 11, 4, 14, 9, 9, 10, 10, 17, 13, 16, 3, 11, 9, 15, 6, 5, 8, 10, 6, 7, 8, 12, 14, 5, 13, 13]
 
-    pcap_parser = PcapParser()
+    nb_file = 0
     for file in os.listdir("data"):
-        params = {
-            'input': 'data/'+file,
-        }
+    #file ="9_7.pcap"
         nb = 0
         cell_id = 0
         #print("Processing File: " + file)
-        for request in pcap_parser.read_pcap(params):
-            if cell_id == 0:
-                cell_id = int(re.search(r'\d+',request.origin).group())
-            else:
-                nb += 1
-        if nb_poi[cell_id] != nb:
+        packets = rdpcap("data/"+file)
+        for pkt in packets:
+            if pkt[TCP].payload:
+                i = pkt[TCP].load.decode('utf-8').find("cell_id=")
+                if i != -1:
+                    cell_id = re.search(r'\d+',pkt[TCP].load.decode('utf-8')[i:]).group()
+                    break
+        for pkt in packets:
+            if pkt[TCP].payload:
+                if "HTTP/1.0 200 OK" in pkt[TCP].load.decode('utf-8'):
+                    nb += 1
+        #print(nb)
+        if nb_poi[int(cell_id)] +1 != nb:
             print(file)
+            nb_file += 1
             if remove:
                 os.remove("data/"+file)
+
+    print(nb_file)
 
 
 def aggregate():
@@ -55,11 +60,11 @@ if __name__ == "__main__":
     if sys.argv[1] == "filter":
         try:
             if sys.argv[2] == "remove":
-                filter(True)
+                filter2(True)
             else:
-                filter(False)
+                filter2(False)
         except Exception:
-            filter(False)
+            filter2(False)
     elif sys.argv[1] == "aggregate":
         aggregate()
     else:
