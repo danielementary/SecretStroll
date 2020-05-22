9a87f67429cd04968f10306862de38f5e0cdd7b9
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2020-05-22 13:57:39 +0200

    modify dict format in pcap.py key id value list of strings

--- skeleton/pcap.py
+++ skeleton/pcap.py
@@ -3,7 +3,6 @@ import sys
 import os
 import re
 
-
 def filter2(remove):
     #None to account cell number from 1 to 100
     nb_poi = [None, 11, 14, 13, 5, 11, 6, 10, 12, 8, 5, 12, 9, 12, 9, 14, 11, 15, 10, 10, 7, 6, 10, 9, 10, 15, 13, 8, 8, 12, 8, 11, 8, 7, 9, 7, 15, 11, 12, 11, 3, 15, 7, 8, 14, 12, 10, 11, 6, 7, 8, 14, 9, 9, 5, 9, 11, 13, 10, 7, 12, 12, 11, 8, 15, 10, 11, 8, 6, 13, 12, 9, 6, 14, 13, 11, 4, 14, 9, 9, 10, 10, 17, 13, 16, 3, 11, 9, 15, 6, 5, 8, 10, 6, 7, 8, 12, 14, 5, 13, 13]
@@ -34,26 +33,28 @@ def filter2(remove):
 
     print(nb_file)
 
-
 def aggregate():
     datas = {}  
 
     for n in range(1,101):
         datas[str(n)] = []
 
-    for file in os.listdir("data"):
-        packets = rdpcap("data/"+file)
+    for file in os.listdir("complete"):
+        packets = rdpcap("complete/"+file)
         for pkt in packets:
-
             if pkt[TCP].payload:
                 i = pkt[TCP].load.decode('utf-8').find("cell_id=")
                 if i != -1:
                     cell_id = re.search(r'\d+',pkt[TCP].load.decode('utf-8')[i:]).group()
                     break
             
+        temp = b''
         for pkt in packets:
             pkt[TCP].remove_payload()
-            datas[cell_id] += [raw(pkt)]
+            temp += raw(pkt)
+
+        datas[cell_id].append(temp)
+
     return datas
 
 if __name__ == "__main__":
@@ -69,5 +70,3 @@ if __name__ == "__main__":
         aggregate()
     else:
         print("unrecognized command!")
-    
-    
